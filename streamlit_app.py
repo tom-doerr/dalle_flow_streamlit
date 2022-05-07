9778da549c69ce8a7bbd33cbe5447fb53e197ed6
Tom Dörr <tom.doerr@tum.de>
2022-05-07 05:08:30 +0200

    Add files

---
+++ streamlit_app.py
@@ -0,0 +1,86 @@
+from docarray import Document
+import streamlit as st
+import time
+# from PIL import Image
+# import requests
+
+SERVER_URL = st.secrets['SERVER_URL']
+LOGO_PATH = 'res/logo.png'
+
+# set the logo and title
+st.set_page_config(page_title="DALL·E Flow Streamlit", initial_sidebar_state="auto", page_icon="res/logo.png")
+
+
+col1, col2, col3 = st.columns([10,1,10])
+with col1:
+    st.title('Logo Creator')
+
+with col2:
+    st.write("")
+
+with col3:
+    st.image(LOGO_PATH, width=200)
+
+num_images = st.sidebar.slider('Number of initial images', 1, 9, 9)
+skip_rate = 1 - st.sidebar.slider('Variations change amount', 0.0, 1.0, 0.5)
+
+st.write('The Logo Creator works best with long descriptions of the logo.')
+st.markdown('Example: `Logo for an environmental startup that contains leaves on a white background.`')
+logo_description = st.text_input('Logo description:')
+
+if not logo_description:
+    st.stop()
+
+
+prompt = logo_description
+
+def get_images(prompt, num_images):
+    return Document(text=prompt).post(SERVER_URL, parameters={'num_images': num_images}).matches
+
+def display_images(images):
+    for i, image in enumerate(images):
+        # d:
+        # <Document ('id', 'adjacency', 'mime_type', 'text', 'uri', 'tags') at f98709a922457dea22a7f19d398e3977>
+        col_left, col_right = st.columns([1,1])
+        with col_left:
+            st.image(image.uri)
+        with col_right:
+            st.button('Create variations', key=image.uri, on_click=diffuse_image, args=(image,))
+            st.button('Create high resolution version', key=image.uri, on_click=upscale_image, args=(image,))
+
+
+def create_initial_image(prompt):
+    start_time = time.time()
+    with st.spinner('Creating the logos. The website uses advanced AI to create the logos. This may take over 10 minutes.'):
+        images = get_images(prompt, num_images)
+    end_time = time.time()
+    st.write(f'Took {end_time - start_time:.1f} seconds')
+    print(f'Took {end_time - start_time:.1f} seconds')
+
+    display_images(images)
+    st.balloons()
+
+def diffuse_image(chosen_image):
+    st.title('Image variations')
+    with st.spinner('Creating variations, this may take a few minutes...'):
+        diffused_images = chosen_image.post(f'{SERVER_URL}', parameters={'skip_rate': skip_rate, 'num_images': 9}, target_executor='diffusion').matches
+
+    display_images(diffused_images)
+    st.stop()
+
+
+def upscale_image(chosen_image):
+    st.title('High resolution image')
+    with st.spinner('Creating a high resolution image from the selected image, this may take a few minutes...'):
+        upscaled_image = chosen_image.post(f'{SERVER_URL}/upscale', target_executor='upscaler')
+    st.image(upscaled_image.uri)
+    st.stop()
+
+def download_image(chosen_image):
+    st.title('Downloading image')
+    st.download_button(chosen_image.uri, chosen_image.uri, mime=chosen_image.mime_type)
+
+    st.stop()
+
+
+create_initial_image(prompt)
